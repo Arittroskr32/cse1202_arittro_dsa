2abce8584178e33e1764f11834c2c237b5f907d3
Arittroskr32 <128909775+Arittroskr32@users.noreply.github.com>
2024-03-29 10:47:28 +0600

    Add files via upload

---
+++ lab1/problem_1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+           
+int main(){
+    int n,k=1;
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d\n",k);
+    }  
+    return 0;
+}
